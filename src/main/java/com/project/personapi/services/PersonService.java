41d87a155ce4498fd04aa7557b836840e379a2e3
Cristie Ferreira <ferreiracristie@gmail.com>
2021-11-05 17:51:55 -0300

    Developed list all method

--- src/main/java/com/project/personapi/services/PersonService.java
+++ src/main/java/com/project/personapi/services/PersonService.java
@@ -8,6 +8,9 @@ import com.project.personapi.repositories.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class PersonService {
     private PersonRepository personRepository;
@@ -27,5 +30,13 @@ public class PersonService {
                 .builder() //passa de uma forma encapsulada e faz o tratamento dos dados de entrada
                 .message("Created person with ID" + savedPerson.getId())
                 .build();
+    }
+
+    public List<PersonDTO> listAll() {
+        List<Person> allpeople = personRepository.findAll();
+        //precisa converter a lista allpeople em personDTO
+        return allpeople.stream() //stream serve para a manipulção e transformação de coleções
+                .map(personMapper::toDTO)//converte os dados
+                .collect(Collectors.toList()); // manda para uma lista
     }
 }
