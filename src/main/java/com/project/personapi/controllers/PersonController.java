9205afa67a3acab420b11ec8e812223f0da7dd80
Cristie Ferreira <ferreiracristie@gmail.com>
2021-11-05 21:18:58 -0300

    Developed get person by ID

--- src/main/java/com/project/personapi/controllers/PersonController.java
+++ src/main/java/com/project/personapi/controllers/PersonController.java
@@ -2,6 +2,7 @@ package com.project.personapi.controllers;
 
 import com.project.personapi.dto.request.PersonDTO;
 import com.project.personapi.dto.response.MessageResponseDTO;
+import com.project.personapi.exeption.PersonNotFoundException;
 import com.project.personapi.services.PersonService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,5 +33,11 @@ public class PersonController {
     @GetMapping
     public List<PersonDTO> listAll(){
         return personService.listAll();
+    }
+
+    @GetMapping("/{id}")
+    public PersonDTO findById(@PathVariable Long id) throws PersonNotFoundException {
+        //pathvariable indica que em uma requisição http você está passando o id de algo
+        return personService.findById(id);
     }
 }
--- src/main/java/com/project/personapi/services/PersonService.java
+++ src/main/java/com/project/personapi/services/PersonService.java
@@ -3,12 +3,15 @@ package com.project.personapi.services;
 import com.project.personapi.dto.request.PersonDTO;
 import com.project.personapi.dto.response.MessageResponseDTO;
 import com.project.personapi.entities.Person;
+import com.project.personapi.exeption.PersonNotFoundException;
 import com.project.personapi.mapper.PersonMapper;
 import com.project.personapi.repositories.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -38,5 +41,15 @@ public class PersonService {
         return allpeople.stream() //stream serve para a manipulção e transformação de coleções
                 .map(personMapper::toDTO)//converte os dados
                 .collect(Collectors.toList()); // manda para uma lista
+    }
+
+    public PersonDTO findById(Long id) throws PersonNotFoundException {
+        Person person = personRepository.findById(id)
+                .orElseThrow(() -> new PersonNotFoundException(id)); // uma maneira diferente de se fazer o código descrito abaixo
+        //Optional<Person> optionalPerson = personRepository.findById(id);
+        //if (optionalPerson.isEmpty()) {
+        //     throw new PersonNotFoundException(id);
+        //}
+        return personMapper.toDTO(person);
     }
 }
